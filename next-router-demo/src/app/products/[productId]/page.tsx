94155e26cbefaba1d5a6c4b593016cc0fbfb811b
Raghuveer <raghuveerp25@gmail.com>
2025-03-11 11:03:17 +0530

    programatically redirect to 404 page

--- next-router-demo/src/app/products/[productId]/page.tsx
+++ next-router-demo/src/app/products/[productId]/page.tsx
@@ -1,8 +1,13 @@
+import { notFound } from 'next/navigation';
+
 const ProductDetails = async (props: {
   params: Promise<{ productId: string }>;
 }) => {
   const params = await props.params;
   const { productId } = params;
+  if (parseInt(productId) > 1000) {
+    notFound();
+  }
   return <h2>Product Details {productId}</h2>;
 };
 
